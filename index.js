b02a48bdbc7b393fb5834e432a1c64e6443112e6
rmaghsoudi <romy_mag@outlook.com>
2019-06-05 16:38:49 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(string){
+  const hours = parseInt(string, 10)
+  
+  if ( hours < 12) return "Good Morning"
+  if ( hours > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
+function displayMessage(string){
+  document.getElementById("greeting").innerText = string;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
